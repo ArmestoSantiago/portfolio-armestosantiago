4715b5d2a17ae9fd173c3f5b1cca8fea9f7cd4f8
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2025-03-17 11:33:09 -0300

    Add english curriculum

--- src/App.jsx
+++ src/App.jsx
@@ -96,7 +96,7 @@ export function App() {
           <div className="external-link" style={{ backgroundColor: lightsOn ? '#000' : '#fff' }}>
             <DownloadArrow lightsOn={lightsOn} />
           </div>
-          <a href='https://drive.google.com/file/d/1AC25Vt8mGSyaDuotDSgAq6fdjYOgtfdw' target='_blank' aria-label='curriculum' rel="noreferrer">
+          <a href={language ? 'https://drive.google.com/file/d/1HjvSEmUp7dmhGsTB1jhORvZRcTTbLell' : 'https://drive.google.com/file/d/1IwhHPjvBnOMw5zakPNrfYmqqvloKjAkm/view'} target='_blank' aria-label='curriculum' rel="noreferrer">
             <CvIncon lightsOn={lightsOn} />
           </a>
         </div >
